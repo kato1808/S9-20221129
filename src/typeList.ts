4b3ad2b790fa5ae4e8c41eb89d9b409ffca05b6f
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-29 17:06:14 +0900

    202211291706

---
+++ src/typeList.ts
@@ -0,0 +1,36 @@
+/* eslint-disable
+
+/** TYPELISTの基本の型 */
+
+// boolean
+let bool: boolean = true;
+
+// number 数値
+let num: number = 0;
+
+// string 文字
+let str: string = "A";
+
+// array 配列
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const func1 = (): void => {
+  const test1 = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefind
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
